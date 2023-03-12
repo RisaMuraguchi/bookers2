a6c6fcd09ca2aef3095ed7539bd68cc696a42ba0
RisaMuraguchi <126639980+RisaMuraguchi@users.noreply.github.com>
2023-03-12 10:11:13 +0000

    課題3Add

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
   resources :books, only: [:new, :index, :show, :edit]
-  resources :users, only: [:index, :show, :edit]
   devise_for :users
   root to: "homes#top"
   get "/home/about" => "homes#about", as: "about"
