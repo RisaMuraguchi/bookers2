5afb755033639eee111be0a4d9181a1eda0c07a8
RisaMuraguchi <126639980+RisaMuraguchi@users.noreply.github.com>
2023-03-12 13:33:56 +0000

    課題3Add

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,12 +1,33 @@
 class BooksController < ApplicationController
 
+  def create
+    @book = Book.new(book_params)
+    @book.user_id = current_user.id
+    if @book.save
+    flash[:notice] ="You have created book successfully."
+    redirect_to book_path(@book.id)
+    else
+      render :index
+    end
+  end
+
   def index
+    @books = Book.all
+    @book = Book.new
   end
 
   def show
+    @book = Book.find(params[:id])
   end
 
   def edit
   end
+
+  private
+
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end
+
 
 end
